7f3e39e7d16d30a1f4d9109a66dcd4c59c903747
sachinshinde1391 <sachinshinde1391@gmail.com>
2015-11-22 18:19:23 -0500

    Completed R code for assignment 2 - Sachin Shinde
    Completed R code for assignment 2 - Sachin Shinde

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,37 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## This function is building the list of basic operations like SET, GET, SETINVERSE, GETINVERS
+  InverseCached <- NULL
+  set <- function(y) {
+    x <<- y
+    InverseCached <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) InverseCached <<- inverse
+  getInverse <- function() InverseCached
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Return a matrix that is the inverse of 'x'
+  ## This function is checking if inverse is already exists in cache
+  ## If not in compute the cache using solve method
+  InverseFunction <- x$getInverse()
+  if(!is.null(InverseFunction)) {
+    ## fetch from cache
+    message("getting cached data")
+    return(InverseFunction)
+  }
+  data <- x$get()
+  ## compute from cache
+  InverseFunction <- solve(data, ...)
+  x$setInverse(InverseFunction)
+  InverseFunction
 }
